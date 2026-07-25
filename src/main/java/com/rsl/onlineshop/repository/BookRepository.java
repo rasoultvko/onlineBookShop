c7520d168df683955981a70175f7cbbcee0fc63e
ahura <rasoultvko@gmail.com>
2026-07-26 01:32:02 +0330

    create project

--- src/main/java/com/rsl/onlineshop/repository/BookRepository.java
+++ src/main/java/com/rsl/onlineshop/repository/BookRepository.java
@@ -1,7 +1,12 @@
 package com.rsl.onlineshop.repository;
 
+import com.rsl.onlineshop.dto.request.BookRequest;
 import com.rsl.onlineshop.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    Optional<Book> findByName (String name);
 }
--- src/main/java/com/rsl/onlineshop/repository/FactorRepository.java
+++ src/main/java/com/rsl/onlineshop/repository/FactorRepository.java
@@ -1,7 +1,12 @@
 package com.rsl.onlineshop.repository;
 
 import com.rsl.onlineshop.model.Factor;
+import com.rsl.onlineshop.model.Payed;
+import com.rsl.onlineshop.model.UserssDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FactorRepository extends JpaRepository<Factor,Long> {
+    Optional<Factor> findByUserssDetailsAndPayed(UserssDetails userssDetails,Payed payed);
 }
